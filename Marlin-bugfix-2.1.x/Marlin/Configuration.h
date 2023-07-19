fe2775857548ffa1c0773e5fce70a5f6c709ab74
disco <milindfernandes@gmail.com>
2023-07-19 10:00:25 +0530

    changed the printable bed area to 100mm

--- Marlin-bugfix-2.1.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.1.x/Marlin/Configuration.h
@@ -667,9 +667,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  16.82
+    #define DEFAULT_Ki   1.42
+    #define DEFAULT_Kd 49.77
   #endif
 #endif
 
@@ -919,21 +919,21 @@
   #endif
 
   // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-  #define DELTA_PRINTABLE_RADIUS 80.0    // (mm)
+  #define DELTA_PRINTABLE_RADIUS 100.0    // (mm)
 
   // Maximum reachable area
-  #define DELTA_MAX_RADIUS       85.0    // (mm)
+  #define DELTA_MAX_RADIUS       110.0    // (mm)
 
   // Center-to-center distance of the holes in the diagonal push rods.
   //#define DELTA_DIAGONAL_ROD 540.0        // (mm)
-  #define DELTA_DIAGONAL_ROD 400.0
+  #define DELTA_DIAGONAL_ROD 396.0
 
   // Distance between bed and nozzle Z home position
   //#define DELTA_HEIGHT 270.00             // (mm) Get this value from G33 auto calibrate
-  #define DELTA_HEIGHT 419.33
+  #define DELTA_HEIGHT 425.0
 
  // #define DELTA_ENDSTOP_ADJ { -3.858, 0.0, -2.496 } // Get these values from G33 auto calibrate
-  #define DELTA_ENDSTOP_ADJ { -8.88, 0.0, -10.69 } // Get these values from G33 auto calibrate
+  #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // Get these values from G33 auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
   #define DELTA_RADIUS 211.89             // (mm) Get this value from G33 auto calibrate 
@@ -941,7 +941,7 @@
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
   // measured in degrees anticlockwise looking from above the printer
-  #define DELTA_TOWER_ANGLE_TRIM { -5.01, 0.85, 4.17 } // Get these values from G33 auto calibrate
+  #define DELTA_TOWER_ANGLE_TRIM { 0.0, 0.0, 0.0 } // Get these values from G33 auto calibrate
 
   // Delta radius and diagonal rod adjustments (mm)
   //#define DELTA_RADIUS_TRIM_TOWER { 0.0, 0.0, 0.0 }
@@ -1498,7 +1498,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -1, -4.22}
+#define NOZZLE_TO_PROBE_OFFSET { -44, -1, -2}
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1558,7 +1558,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-//#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1587,7 +1587,7 @@
 #define Z_PROBE_OFFSET_RANGE_MAX 30
 
 // Enable the M48 repeatability test to test probe accuracy
-//#define Z_MIN_PROBE_REPEATABILITY_TEST
+#define Z_MIN_PROBE_REPEATABILITY_TEST
 
 // Before deploy/stow pause for user confirmation
 //#define PAUSE_BEFORE_DEPLOY_STOW
@@ -1613,9 +1613,9 @@
 //#define DELAY_BEFORE_PROBING 200  // (ms) To prevent vibrations from triggering piezo sensors
 
 // Require minimum nozzle and/or bed temperature for probing
-//#define PREHEAT_BEFORE_PROBING
+#define PREHEAT_BEFORE_PROBING
 #if ENABLED(PREHEAT_BEFORE_PROBING)
-  #define PROBING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
+  #define PROBING_NOZZLE_TEMP 160   // (°C) Only applies to E0 at this time
   #define PROBING_BED_TEMP     50
 #endif
 
@@ -2149,7 +2149,7 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-#define SKEW_CORRECTION
+//#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
